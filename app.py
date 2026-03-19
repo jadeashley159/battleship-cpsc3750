a07c2b8d2446853050f3a44d1288db43a44fe1f2
VipersDrizzy <Aydenmatthew13@gmail.com>
2026-03-19 11:59:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -185,5 +185,8 @@ def get_game(game_id):
 # -------------------------
 # RUN SERVER
 # -------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
