cc856fcac5b22b9edfb6e52867468dbfdd267c85
VipersDrizzy <Aydenmatthew13@gmail.com>
2026-03-31 16:44:21 -0400

    moved the database creation

--- server.py
+++ server.py
@@ -331,9 +331,9 @@ def test_restart(game_id):
 # INIT
 # -------------------------
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
